a95ca3bc0a04f7ec5d1c9622a7200eeac2696685
alenior <alenior@gmail.com>
2024-04-03 00:31:50 -0300

    Relatório.

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -3,6 +3,12 @@ package controller;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,7 +18,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import model.DAO;
 import model.JavaBeans;
 
-@WebServlet(name = "controller", urlPatterns = { "/controller", "/main", "/insert", "/select", "/update" })
+@WebServlet(name = "controller", urlPatterns = { "/controller", "/main", "/insert", "/select", "/update", "/delete",
+		"/report" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
@@ -35,6 +42,10 @@ public class Controller extends HttpServlet {
 			listarContato(request, response);
 		} else if (action.equals("/update")) {
 			editarContato(request, response);
+		} else if (action.equals("/delete")) {
+			removerContato(request, response);
+		} else if (action.equals("/report")) {
+			gerarRelatorio(request, response);
 		} else {
 			response.sendRedirect("index.html");
 		}
@@ -105,14 +116,16 @@ public class Controller extends HttpServlet {
 		RequestDispatcher rd = request.getRequestDispatcher("editar.jsp");
 		rd.forward(request, response);
 	}
-	
+
 	protected void editarContato(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// Teste de recebimento.
-		/**System.out.println(request.getParameter("idcon"));
-		System.out.println(request.getParameter("nome"));
-		System.out.println(request.getParameter("fone"));
-		System.out.println(request.getParameter("email"));**/
+		/**
+		 * System.out.println(request.getParameter("idcon"));
+		 * System.out.println(request.getParameter("nome"));
+		 * System.out.println(request.getParameter("fone"));
+		 * System.out.println(request.getParameter("email"));
+		 **/
 		// Setar as variáveis JavaBeans.
 		contato.setIdcon(request.getParameter("idcon"));
 		contato.setNome(request.getParameter("nome"));
@@ -124,4 +137,56 @@ public class Controller extends HttpServlet {
 		response.sendRedirect("main");
 	}
 
+	// Remover contato.
+	protected void removerContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Recebimento do id do contato a ser excluído (validador.js).
+		String idcon = request.getParameter("idcon");
+		// System.out.println(idcon);
+		// Setar a variável idcon JavaBeans.
+		contato.setIdcon(idcon);
+		// Executar o método deletarContato (DAO) passando o objeto contato.
+		dao.deletarContato(contato);
+		// Redirecionar para o documento agenda.jsp (atualizando as alterações).
+		response.sendRedirect("main");
+	}
+
+	// Gerar relatório em pdf.
+	protected void gerarRelatorio(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Document documento = new Document();
+		try {
+			// Tipo de conteúdo.
+			response.setContentType("apllication/pdf");
+			// Nome do documento.
+			response.addHeader("Content-Disposition", "inline; filename="+ "contatos.pdf");
+			// Criar o documento.
+			PdfWriter.getInstance(documento, response.getOutputStream());
+			// Abrir o documento -> conteúdo.
+			documento.open();
+			documento.add(new Paragraph("Lista de contatos:"));
+			documento.add(new Paragraph(" "));
+			// Criar uma tabela.
+			PdfPTable tabela = new PdfPTable(3);
+			// Cabeçalho.
+			PdfPCell col1 = new PdfPCell(new Paragraph("Nome"));
+			PdfPCell col2 = new PdfPCell(new Paragraph("Fone"));
+			PdfPCell col3 = new PdfPCell(new Paragraph("Email"));
+			tabela.addCell(col1);
+			tabela.addCell(col2);
+			tabela.addCell(col3);
+			// Popular a tabela com os contatos.
+			ArrayList<JavaBeans> lista = dao.listarContatos();
+			for (int i = 0; i < lista.size(); i++) {
+				tabela.addCell(lista.get(i).getNome());
+				tabela.addCell(lista.get(i).getFone());
+				tabela.addCell(lista.get(i).getEmail());
+			}
+			documento.add(tabela);
+			documento.close();
+		} catch (Exception e) {
+			System.out.println(e);
+			documento.close();
+		}
+	}
 }
--- agenda/src/main/webapp/agenda.jsp
+++ agenda/src/main/webapp/agenda.jsp
@@ -20,6 +20,7 @@
 <body>
 	<h1>Agenda de Contatos</h1>
 	<a href="novo.html" class="Botao1">Novo contato</a>
+	<a href="report" class="Botao2">Relatório</a>
 	<table id="tabela">
 		<thead>
 			<tr>
@@ -37,10 +38,13 @@
 					<td><%= lista.get(i).getNome() %></td>
 					<td><%= lista.get(i).getFone() %></td>
 					<td><%= lista.get(i).getEmail() %></td>
-					<td><a href="select?idcon=<%= lista.get(i).getIdcon() %>" class="Botao1">Editar</a></td>
+					<td><a href="select?idcon=<%= lista.get(i).getIdcon() %>" class="Botao1">Editar</a>
+						<a href="javascript: confirmar(<%= lista.get(i).getIdcon() %>)" class="Botao2">Excluir</a>
+					</td>
 				</tr>
 			<%} %>
 		</tbody>
 	</table>
+	<script src="scripts/confirmador.js"></script>
 </body>
 </html>
